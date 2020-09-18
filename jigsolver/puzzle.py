2d823092e2590fa9ffc398c9a3c41631770d48fb
Maun <julien.nonin@sfr.fr>
2020-09-18 20:44:45 +0200

    Create the Piece class attribute: picture,  property: size

---
+++ jigsolver/puzzle.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+class Piece():
+    def __init__(self, picture):
+        picture = np.array(picture)
+        assert picture.ndim == 3, "The picture must be 3-dimensional, i.e. of shape (n,n,3)"
+        assert picture.shape[2] == 3, "Each pixel of the picture must have 3 color values"
+        assert picture.shape[0] == picture.shape[1], "The image must not be rectangular but squared in shape"
+
+        self.picture = picture
+
+    @property
+    def size(self):
+        return len(self.picture)
---
+++ tests/piece_test.py
@@ -0,0 +1,36 @@
+import unittest
+import numpy as np
+from jigsolver.puzzle import Piece
+
+class PieceTestCase(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_piece_size_should_return_size(self):
+        picture = np.array([
+            [[1, 1, 1], [1, 2, 3], [4, 5, 6]], 
+            [[0, 1, 2], [5, 3, 0], [0, 1, 4]],
+            [[8, 5, 1], [4, 5, 0], [8, 5, 1]]
+        ])
+        piece = Piece(picture)
+        return self.assertEqual(piece.size, 3)
+
+    def test_piece_with_rectangular_picture_should_raise_error(self):
+        picture = np.zeros((4, 5, 3))  # RGB picture of shape 4×5
+        with self.assertRaises(AssertionError):
+            piece = Piece(picture)
+
+    def test_piece_without_colored_picture_should_raise_error(self):
+        picture = np.zeros((3, 3, 2))
+        with self.assertRaises(AssertionError):
+            piece = Piece(picture)
+
+    def test_piece_without_three_dimensional_picture_should_raise_error(self):
+        picture = np.zeros((5, 5))
+        with self.assertRaises(AssertionError):
+            piece = Piece(picture)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
