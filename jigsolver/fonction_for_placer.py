1ce31de6bf5cbc866fbd2f1d8714cb404db55fdc
Zhengyang LAN <zylan1227@foxmail.com>
2020-09-26 23:42:11 +0200

    [Fix] function position_to_place adapt to the new class Board in Master

--- jigsolver/fonction_for_placer.py
+++ jigsolver/fonction_for_placer.py
@@ -12,22 +12,22 @@ def find_place_occupied(board):
 
     return list_occupied
 
-def position_to_place(board,n_row,n_column):
-    list_no_none = find_place_occupied(board)
+def position_to_place(board):
+    list_occupied = find_place_occupied(board)
     list_number_position_to_place=[]
-    for e in list_no_none:
+    for e in list_occupied:
 
-        if board[e[0]][e[1]].right_occu == False:
-            list_number_position_to_place = list(set(list_number_position_to_place).union(set([e[0]*n_column+e[1]+1])))
+        if board._grid[e[0]][e[1]].right_occu == False:
+            list_number_position_to_place = list(set(list_number_position_to_place).union(set([e[0]*board.n_cols+e[1]+1])))
 
-        if board[e[0]][e[1]].left_occu == False:
-            list_number_position_to_place = list(set(list_number_position_to_place).union(set([e[0]*n_column+e[1]-1])))
+        if board._grid[e[0]][e[1]].left_occu == False:
+            list_number_position_to_place = list(set(list_number_position_to_place).union(set([e[0]*board.n_cols+e[1]-1])))
 
-        if board[e[0]][e[1]].up_occu == False:
-            list_number_position_to_place = list(set(list_number_position_to_place).union(set([(e[0]-1)*n_column+e[1]])))
+        if board._grid[e[0]][e[1]].up_occu == False:
+            list_number_position_to_place = list(set(list_number_position_to_place).union(set([(e[0]-1)*board.n_cols+e[1]])))
 
-        if board[e[0]][e[1]].down_occu == False:
-            list_number_position_to_place = list(set(list_number_position_to_place).union(set([(e[0]+1)*n_column+e[1]])))
+        if board._grid[e[0]][e[1]].down_occu == False:
+            list_number_position_to_place = list(set(list_number_position_to_place).union(set([(e[0]+1)*board.n_cols+e[1]])))
 
 
     return list_number_position_to_place
