c069c2bbbe6597dae69da193d45cec89bc613ca3
Zhengyang LAN <zylan1227@foxmail.com>
2020-09-27 15:59:04 +0200

    [Add] Function to find in board piece and not in board list number

--- jigsolver/fonction_for_placer.py
+++ jigsolver/fonction_for_placer.py
@@ -4,65 +4,83 @@ from copy import copy
 import matplotlib.pyplot as plt
 
 def find_place_occupied(puzzle):
-    list_occupied=[]
+    list_place_occupied=[]
     n_rows, n_cols = puzzle.board.shape
     for i in np.arange(n_rows):
         for j in np.arange(n_cols):
             if isinstance(puzzle.board.__getitem__([i,j]),Piece):
-                list_occupied.append([i,j])
+                list_place_occupied.append(i*puzzle.board.shape[1]+j)
 
-    return list_occupied
+    return list_place_occupied
 
 def position_to_place(puzzle):
     n_rows, n_cols = puzzle.board.shape
-    list_occupied = find_place_occupied(puzzle)
+    list_place_occupied = find_place_occupied(puzzle)
     list_number_position_to_place=[]
-    for e in list_occupied:
 
-        if puzzle.board[e[0],e[1]].right_occu == False:
-            list_number_position_to_place = list(set(list_number_position_to_place).union(set([e[0]*n_cols+e[1]+1])))
+    for e in list_place_occupied:
 
-        if puzzle.board[e[0],e[1]].left_occu == False:
-            list_number_position_to_place = list(set(list_number_position_to_place).union(set([e[0]*n_cols+e[1]-1])))
+        if puzzle.board[e//n_cols,e%n_cols].right_occu == False:
+            list_number_position_to_place = list(set(list_number_position_to_place).union(set([e+1])))
 
-        if puzzle.board[e[0],e[1]].up_occu == False:
-            list_number_position_to_place = list(set(list_number_position_to_place).union(set([(e[0]-1)*n_cols+e[1]])))
+        if puzzle.board[e//n_cols,e%n_cols].left_occu == False:
+            list_number_position_to_place = list(set(list_number_position_to_place).union(set([e-1])))
 
-        if puzzle.board[e[0],e[1]].down_occu == False:
-            list_number_position_to_place = list(set(list_number_position_to_place).union(set([(e[0]+1)*n_cols+e[1]])))
+        if puzzle.board[e//n_cols,e%n_cols].up_occu == False:
+            list_number_position_to_place = list(set(list_number_position_to_place).union(set([e-n_cols])))
+
+        if puzzle.board[e//n_cols,e%n_cols].down_occu == False:
+            list_number_position_to_place = list(set(list_number_position_to_place).union(set([e+n_cols])))
 
 
     return list_number_position_to_place
 
 
-def find_best_one_piece_to_one_place(board,n_row,n_column,position_number,puzzle,in_space_list):
-    row = position_number//n_column
-    column = position_number % n_column
-    diss_value = []
-    diss_value_list = []
-    not_in_space_list = list(set(list(range(n_row*n_column))).difference(set(in_space_list)))
-    n_average=0
-    for e in not_in_space_list:
-        if column != (n_column-1):
-            if board[row][column+1] is not None:
-                n_average=n_average+1
-                diss_value = puzzle.get_piece(e).diss(board[row][column+1])['R']
-        if column != 0:
-            if board[row][column-1] is not None:
-                n_average = n_average + 1
-                diss_value = diss_value + puzzle.get_piece(e).diss(board[row][column-1])['L']
-        if row !=  n_row-1:
-            if board[row+1][column] is not None:
-                n_average = n_average + 1
-                diss_value = diss_value + puzzle.get_piece(e).diss(board[row][column])['B']
-        if row != 0:
-            if board[row-1][column] is not None:
-                n_average = n_average + 1
-                diss_value = diss_value + puzzle.get_piece(e).diss(board[row][column])['U']
+def find_in_board_pieces(puzzle):
+    list_in_board_pieces = []
+    for i in np.arange(puzzle.board.shape[0]):
+        for j in np.arange(puzzle.board.shape[1]):
+            if isinstance(puzzle.board[i,j],Piece):
+                list_in_board_pieces.append(i*puzzle.board.shape[0]+j)
+    return list_in_board_pieces
 
-        diss_value_list.append(diss_value/n_average)
+def find_not_in_board_pieces(puzzle):
 
-    return not_in_space_list[diss_value_list.index(min(diss_value_list))], min(diss_value_list)
+    return list(set(list(range(puzzle.board.shape[0]*puzzle.board.shape[1]))).difference(set(find_in_board_pieces(puzzle))))
+
+
+
+# def find_best_one_piece_to_one_place(puzzle,position_number):
+#     row = position_number//puzzle.board.shape[0]
+#     column = position_number % puzzle.board.shape[1]
+#     diss_value = []
+#     diss_value_list = []
+#     list_place_occupied = find_place_occupied(puzzle)
+#     not_in_space_pieces_list = list(set(list(range(puzzle.board.shape[0]*puzzle.board.shape[1]))).difference(set(list_occupied)))
+#     n_average=0
+#     print(not_in_space_list)
+
+    # for e in not_in_space_list:
+    #     if column != (n_column-1):
+    #         if board[row][column+1] is not None:
+    #             n_average=n_average+1
+    #             diss_value = puzzle.get_piece(e).diss(board[row][column+1])['R']
+    #     if column != 0:
+    #         if board[row][column-1] is not None:
+    #             n_average = n_average + 1
+    #             diss_value = diss_value + puzzle.get_piece(e).diss(board[row][column-1])['L']
+    #     if row !=  n_row-1:
+    #         if board[row+1][column] is not None:
+    #             n_average = n_average + 1
+    #             diss_value = diss_value + puzzle.get_piece(e).diss(board[row][column])['B']
+    #     if row != 0:
+    #         if board[row-1][column] is not None:
+    #             n_average = n_average + 1
+    #             diss_value = diss_value + puzzle.get_piece(e).diss(board[row][column])['U']
+    #
+    #     diss_value_list.append(diss_value/n_average)
+    #
+    # return not_in_space_list[diss_value_list.index(min(diss_value_list))], min(diss_value_list)
 
 # def decide_piece_to_add():
 #
--- tests/test_placer_temporary.py
+++ tests/test_placer_temporary.py
@@ -13,11 +13,13 @@ eiffel_unsolved = copy(eiffel_puzzle)
 eiffel_unsolved.shuffle()
 
 
-eiffel_unsolved.bag_of_pieces[0].left_occu = True
-eiffel_unsolved.bag_of_pieces[0].up_occu = True
+
 
 some_piece = eiffel_unsolved.get_piece(1)
 eiffel_unsolved.board[0,0] = some_piece
+eiffel_unsolved.board[0,0].left_occu = True
+eiffel_unsolved.board[0,0].up_occu = True
+
 #
 # eiffel_unsolved.display()
 
@@ -37,6 +39,14 @@ Matrix_B[3,0] = Matrix_B[4,1] = Matrix_B[5,2] = 1
 Matrix = {'L':Matrix_L,'R':Matrix_R,'U':Matrix_U,'B':Matrix_B}
 
 
+print(find_place_occupied(eiffel_unsolved))
+print(position_to_place(eiffel_unsolved))
+print(eiffel_unsolved.board[0,0].left_occu)
+
+print(find_in_board_pieces(eiffel_unsolved))
+print(find_not_in_board_pieces(eiffel_unsolved))
+# print(find_best_one_piece_to_one_place(eiffel_unsolved,1))
+
 # n_row = eiffel_puzzle.shape[0]
 # n_column = eiffel_puzzle.shape[1]
 #
@@ -46,17 +56,17 @@ Matrix = {'L':Matrix_L,'R':Matrix_R,'U':Matrix_U,'B':Matrix_B}
 
 # test._grid[0][0].left_occu = True
 # test._grid[0][0].up_occu = True
+
+
 #
-# print(find_place_occupied(eiffel_unsolved))
 #
-# print(position_to_place(eiffel_unsolved))
 # print(position_to_place(test))
 #
 # print(eiffel_puzzle.shape)
 
 # print(eiffel_puzzle.get_compatibilities())
 #
-# print(find_best_one_piece_to_one_place(test,n_row,n_column,18,eiffel_puzzle,in_space_list=[2,3]))
+#
 #
 #
 
