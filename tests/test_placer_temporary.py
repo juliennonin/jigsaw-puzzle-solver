17f1ff38cd273c99a2a24d3fac7cd881a8f730d4
Zhengyang LAN <zylan1227@foxmail.com>
2020-09-27 11:43:20 +0200

    [Test] Test compatibilities matrix

--- tests/test_placer_temporary.py
+++ tests/test_placer_temporary.py
@@ -8,28 +8,28 @@ from jigsolver.fonction_for_placer import *
 
 
 img_real = plt.imread('../img/eiffel.jpg')
-eiffel_puzzle = Puzzle(patch_size=100)
+eiffel_puzzle = Puzzle()
 eiffel_puzzle.create_from_img(img_real)
 eiffel_puzzle.shuffle()
 
 n_row = eiffel_puzzle.shape[0]
 n_column = eiffel_puzzle.shape[1]
 
-test = [[None] * n_column for _ in range(n_row)]
+test=Board(n_row,n_column)
 
-test[2][2]=eiffel_puzzle.get_piece(3)
-test[2][3]=eiffel_puzzle.get_piece(2)
+test.__setitem__([2,3],eiffel_puzzle.get_piece(3))
+test.__setitem__([2,4],eiffel_puzzle.get_piece(4))
 
-test[2][3].left_occu = True
-test[2][2].right_occu = True
+test._grid[2][3].right_occu = True
+test._grid[2][4].left_occu = True
 
-print(n_row,n_column)
+print(position_to_place(test))
 
-print(position_to_place(test,n_row,n_column))
-
-print(find_best_one_piece_to_one_place(test,n_row,n_column,18,eiffel_puzzle,in_space_list=[2,3]))
-
-
+print(eiffel_puzzle.get_compatibilities())
+#
+# print(find_best_one_piece_to_one_place(test,n_row,n_column,18,eiffel_puzzle,in_space_list=[2,3]))
+#
+#
 
 # print(test)
 
