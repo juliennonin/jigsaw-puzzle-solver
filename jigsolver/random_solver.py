1e3c47120065e1b3170d0d96b0e03664af59fc2f
MaxenceGiraud <maxence9giraud@gmail.com>
2020-09-28 18:04:36 +0200

    Adapt random solver to previous refactor

--- jigsolver/random_solver.py
+++ jigsolver/random_solver.py
@@ -1,5 +1,5 @@
 import numpy as np
-from .puzzle import Piece
+from .puzzle import Piece,Slot
 
 def random_solver(puzzle,plotsteps=True):
     '''Solve a puzzle randomly'''
@@ -7,14 +7,16 @@ def random_solver(puzzle,plotsteps=True):
     # Random solver
     np.random.shuffle(puzzle.bag_of_pieces)
 
+    k = 0
     # Simulate solver step by step
     for i in range(puzzle.shape[0]):
         for j in range(puzzle.shape[1]):
-            if puzzle.board[i][j] == None: ## If location empty
-                puzzle.board[i][j] = puzzle.bag_of_pieces[0]
-                puzzle.bag_of_pieces = puzzle.bag_of_pieces[1:]
+            if isinstance(puzzle.board[i,j],Slot): ## If location empty
+                puzzle.board[i,j] = puzzle.bag_of_pieces[k]
+                #puzzle.bag_of_pieces = puzzle.bag_of_pieces[1:] If bag of pieces only contains non placed pieces
 
+                k += 1
                 if plotsteps == True : # Plot puzzle at step
-                    puzzle.plot()
+                    puzzle.display()
 
     return
