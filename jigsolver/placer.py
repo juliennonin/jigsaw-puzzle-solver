c241366c561a23674083f0cbcc77b6505d715d20
Maun <julien.nonin@sfr.fr>
2020-09-28 22:22:08 +0200

    Quick Fix and Test Placer in sandbox

--- jigsolver/placer.py
+++ jigsolver/placer.py
@@ -6,11 +6,15 @@ import matplotlib.pyplot as plt
 
 def naiv_greedy_placer(puzzle, compatibilities, display=False):
     n_pieces = len(list(puzzle.pieces_remaining))
-    n_rows, n_cols = puzzle.shape
-    puzzle.place(puzzle.bag_of_pieces[0], (n_rows//2, n_cols//2))
-    if display:
-        puzzle.display()
-    for _ in range(n_pieces - 1):
+    
+    ## INIT: no piece
+    if all(not piece._is_placed for piece in puzzle.bag_of_pieces):
+        n_rows, n_cols = puzzle.shape
+        puzzle.place(puzzle.bag_of_pieces[0], (n_rows//2, n_cols//2))
+        n_pieces -= 1
+        if display:
+            puzzle.display()
+    for _ in range(n_pieces):
         position, piece = decide_piece_to_add(puzzle, compatibilities)
         puzzle.place(piece, position)
         if display:
@@ -20,12 +24,12 @@ def naiv_greedy_placer(puzzle, compatibilities, display=False):
 def decide_piece_to_add(puzzle, compatibilities):
     positions = list(available_positions(puzzle))
     assert len(positions) != 0, "No empty slot left!"
-    best_value = 0
+    best_value = -1
     best_position = None
     best_piece = None
     for slot_coord in positions:
         piece, value = find_best_piece_for_slot(puzzle,slot_coord,compatibilities)
-        if value > best_value:
+        if value >= best_value:
             best_value = value
             best_position = slot_coord
             best_piece = piece
@@ -33,7 +37,7 @@ def decide_piece_to_add(puzzle, compatibilities):
 
 
 def find_best_piece_for_slot(puzzle, slot_coord, compatibilities):
-    best_diss_value = 0
+    best_diss_value = -1
     best_piece = None
 
     for piece in puzzle.pieces_remaining:
@@ -43,7 +47,7 @@ def find_best_piece_for_slot(puzzle, slot_coord, compatibilities):
                 diss_value.append(compatibilities[piece.id, neigh.id][position])
 
         diss_value_avg = (sum(diss_value) / len(diss_value))
-        if diss_value_avg > best_diss_value:
+        if diss_value_avg >= best_diss_value:
             best_diss_value = diss_value_avg
             best_piece = piece
 
